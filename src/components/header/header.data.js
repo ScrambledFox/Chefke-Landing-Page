c8ac944f888a9a21180b0b8ccc5f80849686f927
Joris Lodewijks <joris.lodewijks@hotmail.com>
2022-05-25 10:36:00 +0200

    Template Init

---
+++ src/components/header/header.data.js
@@ -0,0 +1,23 @@
+import lock from 'assets/images/icons/lock.png';
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'support',
+    label: 'Support',
+  },
+  {
+    path: 'features',
+    label: 'Features',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonials',
+    label: 'Testimonials',
+  },
+];
